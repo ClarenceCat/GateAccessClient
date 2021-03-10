b0839faf482e48dc49d9964da086af514906c0b1
Daniel Grew <Dgrew0287@conestogac.on.ca>
2021-03-10 16:13:50 -0500

    slight modification to the PropertyPage

--- src/pages/UserPages/PropertyPage.js
+++ src/pages/UserPages/PropertyPage.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useProperty } from '../../context/PropertyContext'
 
 export default function PropertyPage() {
 
     const { property_id } = useParams();
+
+    // get the property from the state
+    const { Property } = useProperty()
 
     return (
         <div>
