f9761300299b796e6542ab43785467eac7c89fc5
Daniel Grew <Dgrew0287@conestogac.on.ca>
2021-03-10 23:19:54 -0500

    styled and animated navbar

--- src/components/PropertySideNav/PropertySideNav.js
+++ src/components/PropertySideNav/PropertySideNav.js
@@ -3,26 +3,40 @@
 //      The nav is specific to a given user role within a property
 
 // import css for component
+import {useState} from 'react'
 import { Link } from 'react-router-dom'
 import './PropertySideNav.css'
+import { FaBars } from 'react-icons/fa'
 
 export default function PropertySideNav({property, url, navType}) {
+
+    // useState - used to toggle navbar
+    const [SideBarToggled, setSideBarToggled] = useState(false)
+
+    // toggles side bar
+    const toggleSideBar = () => {
+        setSideBarToggled(!SideBarToggled)
+    }
+
     return (
-        <div className='property-side-nav'>
-            <div className='property-side-nav-header'>
+        <div className={SideBarToggled ? 'property-side-nav' : 'property-side-nav active'}>
+            <div className='side-nav-header'>
+                <FaBars size={'35px'} onClick={() => toggleSideBar()} />
+            </div>
+            <div className='property-side-nav-title'>
                 <h3>{property.name}</h3>
                 <h4>{property.address}</h4>
                 <p>{property.role}</p>
             </div>
-            <div className='property-side-nav-links'>
+            <ul className='property-side-nav-links' onClick={() => {toggleSideBar()}}>
                 {navType.map((navItem, index) => {
                     return (
-                        <div className='property-side-nav-item'>
-                            <Link to={`${url}${navItem.route}`} key={index}>{navItem.name}</Link>
-                        </div>
+                        <li className='property-side-nav-item' key={index}>
+                            <Link to={`${url}${navItem.route}`} >{navItem.name}</Link>
+                        </li>
                         )
                 })}
-            </div>
+            </ul>
         </div>
     )
 }
