865d8b774a9fcddf461b8c31ff03d001cb62bcf0
Daniel Grew <Dgrew0287@conestogac.on.ca>
2021-04-16 13:52:53 -0400

    made login responsive

--- src/pages/HeroPages/HeroPage.js
+++ src/pages/HeroPages/HeroPage.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { MdClose, MdMenu } from 'react-icons/md'
 import {BrowserRouter as Router, Switch, Route, Link, Redirect} from 'react-router-dom'
 import LoginPage from './LoginPage'
 import RegisterPage from './RegisterPage'
 import './styles/HeroPage.css'
 
 export default function HeroPage() {
+
+    const [NavOpen, setNavOpen] = useState(false)
+
+    function navClick() {
+        setNavOpen(!NavOpen)
+    }
     
     return (
         <Router>
@@ -15,9 +22,12 @@ export default function HeroPage() {
                     <div className='visitor-nav-title'>
                         <h3>Gate <span>Access Management Systems</span></h3>
                     </div>
-                    <div className='visitor-nav-routes'>
-                        <Link to='/'>Login</Link>
-                        <Link to='/register'>Sign Up</Link>
+                    <div className="menu-icon" onClick={navClick}>
+                        {NavOpen ? <MdClose /> : <MdMenu />}
+                    </div>
+                    <div className={NavOpen ? 'visitor-nav-routes active' : 'visitor-nav-routes'}>
+                        <Link onClick={navClick} to='/'><span className="nav-links">Login</span></Link>
+                        <Link onClick={navClick} to='/register'><span className="nav-links">Sign Up</span></Link>
                     </div>
                 </div>
                 <div className='content'>
