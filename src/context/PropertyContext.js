d93fe8d62609b2a8ab8b5bdbfa1aa53dd2d7afb4
Daniel Grew <Dgrew0287@conestogac.on.ca>
2021-04-19 18:24:25 -0400

    added comments

--- src/context/PropertyContext.js
+++ src/context/PropertyContext.js
@@ -1,12 +1,22 @@
+// File: PropertyContext.js
+// Description: This file contains the context setup for the Property Context
+//  This context is used to hold property information so it can be accessed accross the
+//  User dashboards
+
 import { createContext, useReducer, useContext } from 'react'
 import { propertyReducer } from '../reducers/propertyReducer'
 
 export const PropertyContext = createContext()
 
+// Function: useProperty()
+// Description: This function is called to access the property context
+// Returns: an instance of the property context
 export const useProperty = () => {
     return useContext(PropertyContext)
 }
 
+// Component: PropertyContextProvider
+// Description: any component wrapped inside of this will have access to the property context 
 export default function PropertyContextProvider({children}) {
 
     const [Property, propertyDispatch] = useReducer(propertyReducer, null)
--- src/reducers/propertyReducer.js
+++ src/reducers/propertyReducer.js
@@ -1,3 +1,6 @@
+// File: propertyReducer.js
+// Description: This contains the reducer used to perform actions on the property context
+
 export const propertyReducer = (state, action) => {
     // switch statement used to perform action
     switch (action.type) {
